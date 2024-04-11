d3d4d75a87f052b9587275d78254ecef916d878c
aladincreater <aladincreater@gmail.com>
2024-04-11 22:20:29 +0530

    Bean LifeCycle Implementation.

---
+++ src/main/java/com/SpringBootLearning/BeanLifeCycle/Common/CricketCoach.java
@@ -0,0 +1,23 @@
+package com.SpringBootLearning.BeanLifeCycle.Common;
+
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+//Scope Implementation
+//@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+public class CricketCoach implements Coach {
+    @Override
+    public String getDailyWorkout() {
+        return "Do Fast Bowling for 15 Mins";
+    }
+}
+
+//@Component
+//public class CricketCoach implements Coach {
+//    @Override
+//    public String getDailyWorkout() {
+//        return "Do Fast Bowling.Do Batting for 15 Mins";
+//    }
+//}
